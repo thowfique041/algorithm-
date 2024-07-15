243766e854c6b3b5ea9fab9609cd2239d62a9e4c
Thowfiqur bari chowdhury <138692245+thowfique041@users.noreply.github.com>
2024-07-15 22:01:53 +0600

    Create Longest prefix suffix.cpp

---
+++ Longest prefix suffix.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    // Convert the input string to uppercase
+    transform(s.begin(), s.end(), s.begin(), ::toupper);
+
+    int n = s.size();
+    vector<int> v(n, 0);
+
+    int j = 1;
+    int i = 0;
+    while (j < n) {
+        if (s[i] == s[j]) {
+            v[j] = i + 1;
+            i++;
+            j++;
+        } else {
+            if (i == 0) {
+                v[j] = 0;
+                j++;
+            } else {
+                i = v[i - 1];
+               
+            }
+        }
+    }
+    
+
+    cout << v[v.size() - 1] << endl;
+    return 0;
+}
